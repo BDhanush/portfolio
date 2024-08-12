d2def1f6bf38cb41dbdcae2fee22de71e61a5da4
Dhanush <dhanushbommavaram@gmail.com>
2024-08-12 18:07:37 +0530

    leetcode update

--- src/data/cpLinksData.ts
+++ src/data/cpLinksData.ts
@@ -1,6 +1,6 @@
 export const cpItems = [
     { link: 'https://codeforces.com/profile/bdhanush', title: 'codeforces/bdhanush', ratingInfo: 'Specialist (1573)' },
-    { link: 'https://leetcode.com/dhanushbommavaram/', title: 'leetcode/dhanushbommavaram', ratingInfo: 'Knight, Top 3.41% (1955)' },
+    { link: 'https://leetcode.com/dhanushbommavaram/', title: 'leetcode/dhanushbommavaram', ratingInfo: 'Knight, Top 3.39% (1955)' },
     { link: 'https://www.codechef.com/users/b_dhanush/', title: 'codechef/b_dhanush', ratingInfo: '1749' },
     { link: 'https://www.facebook.com/codingcompetitions/hacker-cup/2023/certificate/148853454064997', title: 'Meta Hackercup', ratingInfo: 'rank: 2297 out of 20324' },
 ]
