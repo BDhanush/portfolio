38d3a885da857e09c5ee43142f93952489418f3d
Dhanush <dhanushbommavaram@gmail.com>
2024-08-20 09:44:58 +0530

    about update

--- src/data/AboutData.ts
+++ src/data/AboutData.ts
@@ -1,7 +1,7 @@
 export const authorName = "Dhanush Bommavaram"
 
 export const introHeader = 
-"An incoming mscs student at UIC looking for software engineering opportunities"
+"A MSCS student at University of Illinois, Chicago looking for software engineering opportunities"
 
 export const introText = 
-"I'm a passionate software developer with a strong background in Android and backend development. I love creating innovative, user-friendly, robust, and scalable software. Continuously learning and exploring new technologies, I strive to deliver high-quality code and innovative solutions that exceed expectations."
+"I'm a passionate software developer with a strong background in Android and backend development. I love creating innovative, user-friendly, and robust software. Continuously learning and exploring new technologies, I strive to deliver high-quality code and innovative solutions that exceed expectations."
