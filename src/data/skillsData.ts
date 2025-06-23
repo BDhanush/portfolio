3d7036440f20bf7f1a93d160a8e331ce980c128c
Dhanush <dhanushbommavaram@gmail.com>
2025-06-23 01:16:48 -0500

    internship update

--- src/data/skillsData.ts
+++ src/data/skillsData.ts
@@ -1,5 +1,5 @@
 export const skillsItems= [
-    'C++','C', 'Kotlin', 'Python', 'Java',
+    'C++','C', 'Kotlin', 'Python', 'Java', 'Go',
     'Android Development', 'Git', 'Rest API', 'SQL',  'MySQL', 'Firebase', 'MongoDB', 'Web Automation', 'Web Scraping', 'TypeScript', 'React', 'Angular', 'Django',
     // 'Django', 'Spring Boot', 'Angular', 'React'
 ]
